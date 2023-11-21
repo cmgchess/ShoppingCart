1e27a969bc4aebf427690d939f7af05431aabece
cmgchess <chathulanka.18@cse.mrt.ac.lk>
2023-11-21 14:07:22 +0530

    2 - encapsulate

--- src/main/java/com/example/shop/ShoppingCart.java
+++ src/main/java/com/example/shop/ShoppingCart.java
@@ -4,12 +4,19 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ShoppingCart {
-    public List<Product> cartItems;
+    private List<Product> cartItems;
 
     public ShoppingCart() {
         this.cartItems = new ArrayList<Product>();
     }
 
+    public List<Product> getCartItems() {
+        return cartItems;
+    }
+
+    public void setCartItems(List<Product> cartItems) {
+        this.cartItems = cartItems;
+    }
 
     public void removeItem(Product product){
         //TODO
@@ -22,7 +29,7 @@ public class ShoppingCart {
     public double calculateTotalPrice() {
         double total = 0;
         for (Product product: cartItems) {
-            total += product.price;
+            total += product.getPrice();
         }
         return total;
     }
@@ -30,7 +37,7 @@ public class ShoppingCart {
     public void printCart(){
         System.out.println("Printing Cart Items");
         for (Product product: cartItems) {
-            System.out.println(product.name + " => Price: " + product.price + ", Quantity: " + product.quantity);
+            System.out.println(product.getName() + " => Price: " + product.getPrice() + ", Quantity: " + product.getQuantity());
         }
     }
 
